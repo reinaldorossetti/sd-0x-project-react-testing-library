d73b843115e11a206946536d199abf6d803f21fc
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-12-03 08:20:20 -0200

    Eslint done

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -13,9 +13,9 @@ const Pokemon = ({ pokemon, showDetailsLink, isFavorite }) => {
   return (
     <div className="pokemon">
       <div className="pokemon-overview">
-        <p data-testid="pokemon-name">{`${name}`}</p>
-        <p data-testid="pokemonType">{`${type}`}</p>
-        <p data-testid="pokemon-weight">
+      <p data-testid={`pokemon-name`}>{`${name}`}</p>
+        <p data-testid={`pokemon-type`}>{`${type}`}</p>
+        <p data-testid={`pokemon-weight`}>
           {`Average weight: ${value} ${measurementUnit}`}
         </p>
         {showDetailsLink && <Link to={ `pokemons/${id}` }>More details</Link>}
